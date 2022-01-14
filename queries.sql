17fd779262d2565eea3495ddaddb94bef7d69347
Chike1990 <chibuikeuzoechina@gmail.com>
2022-01-14 17:36:40 +0100

    Write queries using JOIN

--- queries.sql
+++ queries.sql
@@ -47,3 +47,61 @@ FROM animals
 WHERE EXTRACT(YEAR FROM date_of_birth) 
 BETWEEN 1990 AND 2000 
 GROUP BY species;
+
+
+-- Write queries (using JOIN) to answer the following questions:
+
+-- What animals belong to Melody Pond?
+SELECT o.full_name, a.name 
+FROM owners o 
+INNER JOIN animals a 
+ON o.owner_id = a.owners_id
+WHERE o.full_name LIKE 'Melody Pond';
+
+-- List of all animals that are pokemon 
+SELECT a.name 
+FROM species s 
+INNER JOIN animals a
+ON s.specie_id = a.species_id
+WHERE s.name LIKE 'Pokemon';
+
+-- List all owners and their animals (remember to include those that don't own any animal)
+SELECT o.full_name, a.name
+FROM owners o
+LEFT JOIN animals a
+ON o.owner_id = a.owners_id;
+
+-- How many animals are there per species?
+SELECT s.name, COUNT(a.species_id) As num
+FROM species s
+FULL JOIN animals a
+ON s.specie_id = a.species_id
+GROUP BY s.name;
+
+-- List all Digimon owned by Jennifer Orwell
+SELECT o.full_name, a.name AS Digimon
+FROM owners o
+LEFT JOIN animals a
+ON o.owner_id = a.owners_id
+LEFT JOIN species S
+ON S.specie_id = a.species_id
+WHERE o.full_name LIKE 'Jennifer Orwell'
+AND S.name LIKE 'Digimon'
+
+-- List all animals owned by Dean Winchester that haven't tried to escape
+SELECT o.full_name, a.name
+FROM owners O 
+INNER JOIN animals A
+ON O.owner_id = A.owners_id
+INNER JOIN species S
+ON S.specie_id = A.species_id
+WHERE full_name LIKE 'Dean Winchester'
+AND A.escape_attempts = 0;
+
+-- Who owns the most animals?
+SELECT O.full_name, COUNT(*) 
+FROM owners O
+JOIN animals A
+ON O.owner_id = A.owners_id
+GROUP BY O.full_name
+HAVING COUNT(*)> 2;
